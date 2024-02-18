be1679d5ebaaa5ddda28a305e1266f2e1552fff2
MarcoL087 <148775781+MarcoL087@users.noreply.github.com>
2024-02-18 14:08:45 +0100

    Add files via upload

---
+++ dash2.py
@@ -0,0 +1,112 @@
+import dash
+from dash import dcc, html, Input, Output, State
+import plotly.express as px
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv('https://raw.githubusercontent.com/chriszapp/datasets/main/books.csv', on_bad_lines='skip')
+top_10_books = df.head(10)
+
+# Initial figure
+initial_fig = px.bar(
+    top_10_books,
+    y='title',
+    x='  num_pages',
+    title='Top 10 Books - Number of pages per title',
+    orientation='h',
+    labels={'  num_pages': 'Number of Pages'},
+    height=600,
+    width=1200
+)
+
+initial_fig.update_traces(marker_color='#fcba6f')
+
+initial_fig.update_layout(
+    font_color='rgba(255, 255, 255, 0.9)',
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    yaxis_title='',
+    margin=dict(pad=25)
+)
+
+# Initialize Dash app
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
+
+# App layout
+app.layout = html.Div(
+    style={
+        'background-image': 'url("/assets/background.jpg")',
+        'background-size': 'cover',
+        'background-position': 'center',
+        'flexDirection': 'column',
+        'alignItems': 'center',
+        'justifyContent': 'center',
+        'padding': '0 150px',
+        'color': 'rgba(255, 255, 255, 0.9)',
+    },
+    children=[
+        html.Img(src=app.get_asset_url('Resource-Page-Books-banner.png'), style={'width': '100%', 'max-width': '1300px'}),
+        html.H1("Top 10 Books", style={'text-align': 'center', 'font-size': '36px', 'font-family': 'system-ui', 'margin': '80px'}),
+        dcc.Graph(figure=initial_fig, id='book-graph', style={'background-color': 'transparent', 'margin-bottom': '30px'}),
+
+        html.H1("Select your author/ number of pages", style={'text-align': 'center', 'font-size': '36px', 'font-family': 'system-ui', 'margin': '80px'}),
+        html.Label("Select an author:", style={'font-family': 'system-ui', 'margin-top': '50px', 'margin-bottom': '10px'}),
+        dcc.Dropdown(
+            id='author-dropdown',
+            options=[{'label': author, 'value': author} for author in df['authors'].unique()],
+            multi=True,
+            style={'color': '#2a2a2b'}
+        ),
+        html.Label("Enter a maximum number of pages:", style={'font-family': 'system-ui', 'margin-top': '10px', 'margin-bottom': '10px'}),
+        dcc.Input(
+            id='max-pages-input',
+            type='number',
+            value=df['  num_pages'].max()
+        ),
+        html.Button('Update Graph', id='update-button', n_clicks=0, style={'margin-bottom': '50px'}),
+
+        html.Div(id='book-graph-container', style={'display': 'none', 'margin-bottom': '80px'}),
+    ]
+)
+
+# Callback 
+
+@app.callback(
+    [Output('book-graph-container', 'children'),
+     Output('book-graph-container', 'style')],
+    [Input('update-button', 'n_clicks')],
+    [State('author-dropdown', 'value'),
+     State('max-pages-input', 'value')]
+)
+def update_graph(n_clicks, selected_author, max_pages):
+    if n_clicks > 0:
+        filtered_df = df.copy()
+        if selected_author:
+            filtered_df = filtered_df[filtered_df['authors'].isin(selected_author)]
+        filtered_df = filtered_df[filtered_df['  num_pages'] <= max_pages]
+
+        updated_fig = px.bar(
+            filtered_df,
+            y='title',
+            x='  num_pages',
+            title='Book by author and number of pages',
+            orientation='h',
+            labels={'  num_pages': 'Number of Pages'},
+            height=600,
+            width=1200
+        )
+        updated_fig.update_traces(marker_color='#fcba6f')
+        updated_fig.update_layout(
+            font_color='rgba(255, 255, 255, 0.9)',
+            paper_bgcolor='rgba(0,0,0,0)',
+            plot_bgcolor='rgba(0,0,0,0)',
+            yaxis_title='',
+            margin=dict(pad=85)
+        )
+
+        return [dcc.Graph(figure=updated_fig, id='book-graph')], {'display': 'block'}
+
+    return [], {'display': 'none'}
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
